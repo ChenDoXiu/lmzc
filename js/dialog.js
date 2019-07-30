e616d8c1f9906d39ac3b9ae9c3a48e978c00647a
chenhaonan520 <2839673621@qq.com>
2019-07-31 00:44:49 +0800

    弹窗完善

--- js/dialog.js
+++ js/dialog.js
@@ -1,3 +1,5 @@
+
+try{
 function Dialog(obj){
 	obj = obj || {};
 	this.type = obj.type || 0;
@@ -5,9 +7,13 @@ function Dialog(obj){
 	this.titleAligin = obj.titleAligin || "center";
 	this.body = obj.body || null;
 	this.footer = obj.foot || null;
+	this.allObject.push(this);
+	this.box = null;
 }
 //
-Dialog.prototype.popup = function(){
+Dialog.prototype.allObject = [];
+Dialog.prototype.rander = function(node){
+	node = node || document.body;
 	switch(this.type){
 		case 0:
 			this.type = "dialog";
@@ -25,6 +31,53 @@ Dialog.prototype.popup = function(){
 			this.type = "dialog";
 		break;
 	}
-	var box = $("<div></div>").addClass(this.type);;
+	var box = $("<div></div>").addClass(this.type);
+	box.append($("<div></div>").addClass("dialog-title").addClass(this.titleAligin).html(this.title));
+	box.append($("<div></div>").addClass("dialog-body").html(this.body));
+	box.append($("<div></div>").addClass("dialog-footer").html(this.footer));
+	box.css("display","none");
+	this.box = box;
+	$(node).append(this.box);
+};
+Dialog.prototype.show = function(){
+	
+	this.box.css("display","block");
+	this.box.animate({
+		right:"-=300",
+		opacity:'0'
+	},0).animate({
+		right:"+=300",
+		opacity:"1"
+	},400);
+};
+Dialog.prototype.hide = function(){
+	this.box.animate({
+		right:"-=300",
+		opacity:'1'
+	},400).animate({
+		right:"+=300",
+		opacity:"0"
+	},0,function(){
+		$(this).hide(0);
+	});
+	//this.box.css("display","none");
+};
+Dialog.prototype.setTitle = function(node){
+	this.title = node;
+	this.box.find(".dialog-title").html(this.title);
+};
+Dialog.prototype.setBody = function(node){
+	this.body = $(node);
+	this.box.find(".dialog-body").html(this.body);
+};
+Dialog.prototype.setFooter = function(node){
+	this.footer = $(node);
+	this.box.find(".dialog-footer").html(this.footer);
+};
+Dialog.prototype.appendBody = function(node){
+	this.body.append(node);
 	
 };
+}catch(e){
+	alert(e);
+}
--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
+window.onload = function(){
 function main(){
 	setSelect();
+	var test = new Dialog({
+		type:1,
+		title:"ueuejeh"
+	});
+	test.rander();
+	test.show();
+	test.setTitle("23333");
+	test.setBody($("<div class='text-box text-center'>123</div>"));
+	//test.hide();
 }
 main();
+};
